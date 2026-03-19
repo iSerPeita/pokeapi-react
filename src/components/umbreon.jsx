1a53921479477d8ea8af8145af892f10bd07d304
iSer <isaac.sanguino91@gmail.com>
2026-03-19 20:03:03 +0100

    Limpieza inicial: base minima de la app y un umbreon muy majete

---
+++ src/components/umbreon.jsx
@@ -0,0 +1,12 @@
+import umbreonImg from '../assets/umbreon_shiny.png';
+import './umbreon.css'
+
+function Umbreon (){
+    return (
+        <div>
+          <img src={umbreonImg} alt="umbreon" className="unbrImg" />
+        </div>
+      )
+}
+
+export default Umbreon
